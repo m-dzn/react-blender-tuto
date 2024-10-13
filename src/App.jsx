f6d2849d09b66b47879e3138959a15909115690d
m-dzn <mdzn.dev@gmail.com>
2024-10-14 04:25:48 +0900

    feat: draw Isometric 3D room

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,137 @@
-import { useLoader, Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 
 import "./App.css";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, OrthographicCamera } from "@react-three/drei";
 import Model from "./components/Model";
+import { useControls } from "leva";
+import { useEffect, useRef, useState } from "react";
 
-function App() {
+import monitorDisplayVertexShader from "./shaders/vertex.glsl?raw";
+import monitorDisplayFragmentShader from "./shaders/fragment.glsl?raw";
+
+const App = () => {
+  const { x, y, z } = useControls("Model Position", {
+    x: { value: 0, min: -100, max: 100 },
+    y: { value: -10, min: -100, max: 100 },
+    z: { value: 0, min: -100, max: 100 },
+  });
+
   return (
     <div className="App">
       <Canvas>
-        <OrbitControls />
-        <ambientLight intensity={1} />
-        <directionalLight position={[10, 10, 5]} intensity={10} />
+        <Camera />
 
-        <Model url={"/assets/models/table.glb"} />
-        <Model
-          url={"/assets/models/chair.glb"}
-          position={[11, 1, 0]}
-          scale={[1.7, 1.7, 1.7]}
-        />
-        <Model
-          url={"/assets/models/desk.glb"}
-          position={[10, 3, 6]}
-          scale={[7, 7, 7]}
-          rotation={[0, Math.PI, 0]}
-        />
-        <Model
-          url={"/assets/models/cubi.glb"}
-          position={[25, 3, 0]}
-          scale={[4, 4, 4]}
-          rotation={[0, (-Math.PI * 3) / 8, 0]}
-        />
+        <Lightings />
+
+        <Model url={"/assets/models/home.glb"} position={[x, y, z]} />
       </Canvas>
     </div>
   );
-}
+};
+
+const Camera = () => {
+  const { camera } = useThree();
+
+  const { cameraX, cameraY, cameraZ } = useControls("Camera Position", {
+    cameraX: { value: 100, min: 0, max: 200 },
+    cameraY: { value: 100, min: 0, max: 200 },
+    cameraZ: { value: 100, min: 0, max: 200 },
+  });
+
+  useEffect(() => {
+    camera.position.set(cameraX, cameraY, cameraZ);
+  }, [cameraX, cameraY, cameraZ, camera]);
+
+  return (
+    <>
+      <OrthographicCamera makeDefault zoom={15} near={0.1} far={1000} />
+
+      <OrbitControls />
+    </>
+  );
+};
+
+const Lightings = () => {
+  const lightRef = useRef();
+
+  const { lightX, lightY, lightZ, intensity } = useControls("Light Position", {
+    lightX: { value: 2, min: -10, max: 10 },
+    lightY: { value: 10, min: -10, max: 10 },
+    lightZ: { value: -2, min: -10, max: 10 },
+    intensity: { value: 5, min: -2, max: 50, step: 0.1 },
+  });
+
+  const { scene } = useThree();
+  const [bulbLight, setBulbLight] = useState();
+
+  useEffect(() => {
+    scene.traverse((child) => {
+      if (child.name === "stand") {
+        const pointLight = (
+          <pointLight
+            position={child.position.clone()}
+            intensity={100}
+            scale={{ x: 0.001, y: 0.001, z: 0.001 }}
+          />
+        );
+
+        setBulbLight(pointLight);
+      }
+
+      if (child.name === "monitor_display") {
+        child.material = (
+          <shaderMaterial
+            vertexShader={monitorDisplayVertexShader}
+            fragmentShader={monitorDisplayFragmentShader}
+          />
+        );
+      }
+    });
+  }, [scene]);
+
+  useEffect(() => {
+    if (lightRef.current) {
+      lightRef.current.position.set(lightX, lightY, lightZ);
+      lightRef.current.intensity = intensity;
+    }
+  }, [lightX, lightY, lightZ, intensity]);
+
+  return (
+    <>
+      <ambientLight intensity={1} />
+      <directionalLight
+        ref={lightRef}
+        position={[10, 10, 5]}
+        intensity={intensity}
+        castShadow
+      />
+
+      {bulbLight}
+    </>
+  );
+};
+
+const Studio = () => {
+  return (
+    <>
+      <mesh scale={[50, 1, 50]} position={[8, -1, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color="#aaa" />
+      </mesh>
+      <mesh scale={[50, 25, 1]} position={[8, 11, -25]}>
+        <boxGeometry />
+        <meshStandardMaterial color="#aaa" />
+      </mesh>
+      <mesh
+        scale={[50, 25, 1]}
+        position={[33, 11, 0]}
+        rotation={[0, Math.PI / 2, 0]}
+      >
+        <boxGeometry />
+        <meshStandardMaterial color="#aaa" />
+      </mesh>
+    </>
+  );
+};
 
 export default App;
--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,7 +1,8 @@
 import { useGLTF } from "@react-three/drei";
 
 const Model = ({ url, position, ...rest }) => {
-  const gltf = useGLTF(url);
+  const gltf = useGLTF(url, true);
+
   return <primitive object={gltf.scene} position={position} {...rest} />;
 };
 
